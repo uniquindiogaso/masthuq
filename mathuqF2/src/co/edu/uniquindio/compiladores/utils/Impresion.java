6bd42a179abf07885acaae36e290724c18512cd9
gaso <gustavo2840@gmail.com>
2018-03-25 13:03:40 -0500

    Avanzando en Implementacion de recuperacion de Errores

--- mathuqF2/src/co/edu/uniquindio/compiladores/utils/Impresion.java
+++ mathuqF2/src/co/edu/uniquindio/compiladores/utils/Impresion.java
@@ -2,9 +2,12 @@ package co.edu.uniquindio.compiladores.utils;
 
 import java.util.ArrayList;
 
+import co.edu.uniquindio.compiladores.frontend.lexico.ParseException;
+
 public class Impresion {
 	
 	ArrayList<Variable> variables;
+	ArrayList<ParseException> errores;
 	
 	public static String ImprimirLexico(ArrayList<Variable> vars){
 		//System.out.println(n + ") Linea:\u005ct"+nL+"\u005ctColumna:\u005ct"+nC+"\u005ctLexema:\u005ct\u005c""+lexema+"\u005c"\u005ct"+"Token:\u005ct"+token);
@@ -16,7 +19,11 @@ public class Impresion {
 	}
 	
 	public void test(){
-		variables.add(new Variable(num, token, pieza));
+		//variables.add(new Variable(num, token, pieza));
+		  ParseException error = null;
+		  
+		  String msj = "Linea " +  error.currentToken.beginLine  + " Columna " + error.currentToken.beginColumn + " Encontrado " + error.tokenImage[0];
+		
 	}
 
 	public ArrayList<Variable> getVariables() {
@@ -26,6 +33,17 @@ public class Impresion {
 	public void setVariables(ArrayList<Variable> variables) {
 		this.variables = variables;
 	}
+
+	public ArrayList<ParseException> getErrores() {
+		return errores;
+	}
+
+	public void setErrores(ArrayList<ParseException> errores) {
+		this.errores = errores;
+	}
+	
+	
+	
 	
 	
 
